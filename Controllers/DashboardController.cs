e4e1417df8449836b7a1e656c9496014dc59e28d
hieuhieu2202 <hieuhieu22022001@gmail.com>
2024-09-30 17:59:54 +0700

    Done phần search

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -15,7 +15,7 @@ namespace TestZCESD.Controllers
         {
             using (var client = new HttpClient())
             {
-                var response = await client.GetAsync("http://10.220.130.116:2222/APIBYHIEU/api/ESDFace/GetEmployeeData?workshop=F06&floor=2F");
+                var response = await client.GetAsync("http://10.220.130.116:2222/APIBYHIEU/api/ESDFace/GetEmployeeData?workshop=F06&floor=1F");
                 var content = await response.Content.ReadAsStringAsync();
                 return Content(content, "application/json");
             }
